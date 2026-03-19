9dc127bda2da64ae736599aa3a9abc7b1eb909d0
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2026-03-19 09:40:21 -0300

    chore: init express app and basic server

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Olá mundo!");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+    if(error){
+        throw error;
+    }
+
+    console.log(`Listening on port ${PORT}`);
+});
